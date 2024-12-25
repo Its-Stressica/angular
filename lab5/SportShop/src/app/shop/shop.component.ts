34a3452799c93746549724523e3d545aa4480a47
Its-Stressica <itsgogolsophia@gmail.com>
2024-12-25 22:57:16 +0200

    lab 5 part (add "add to cart" button)

--- lab5/SportShop/src/app/shop/shop.component.ts
+++ lab5/SportShop/src/app/shop/shop.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Product } from '../model/product.model';
 import { ProductRepository } from '../model/product.repository';
+import { Cart } from '../model/cart.model';
 @Component({
   selector: 'shop',
   templateUrl: 'shop.component.html',
@@ -9,7 +10,7 @@ export class ShopComponent {
   selectedCategory: string | undefined;
   productsPerPage = 4;
   selectedPage = 1;
-  constructor(private repository: ProductRepository) {}
+  constructor(private repository: ProductRepository, private cart: Cart) {}
   get products(): Product[] {
     let pageIndex = (this.selectedPage - 1) * this.productsPerPage;
     return this.repository
@@ -38,5 +39,14 @@ export class ShopComponent {
     )
       .fill(0)
       .map((x, i) => i + 1);
+  }
+  get pageCount(): number {
+    return Math.ceil(
+      this.repository.getProducts(this.selectedCategory).length /
+        this.productsPerPage
+    );
+  }
+  addProductToCart(product: Product) {
+    this.cart.addLine(product);
   }
 }
--- lab5/SportShop/src/app/shop/shop.module.ts
+++ lab5/SportShop/src/app/shop/shop.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { ModelModule } from '../model/model.module';
 import { ShopComponent } from './shop.component';
+import { CounterDirective } from './counter.directive';
+import { CartSummaryComponent } from './cartSummary.component';
 @NgModule({
   imports: [ModelModule, BrowserModule, FormsModule],
-  declarations: [ShopComponent],
+  declarations: [ShopComponent, CounterDirective, CartSummaryComponent],
   exports: [ShopComponent],
 })
 export class ShopModule {}
